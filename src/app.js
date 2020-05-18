a116176d300758f11ed0d09db6abf7ab943e181f
Caleb335 <belac335@gmail.com>
2020-05-18 12:00:33 +0100

    added routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/SignUp';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
   return (
-    <div>
-      <h1>BuildForSDG</h1>
-    </div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
